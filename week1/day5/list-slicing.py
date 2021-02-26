a1036842274c43eb697a2e5f1fa10fc8947f8c45
HassanBahati <mukisabahati@gmail.com>
2021-02-26 17:29:02 +0300

    added list-slicing

---
+++ week1/day5/list-slicing.py
@@ -0,0 +1,14 @@
+#list slicing 
+Nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#prints starting from index 2
+print(Nums[2:])
+#prints and stops at index 1 before index 2
+print(Nums[:2])
+
+
+'''
+QUIZ
+define a list of numbers 1-10
+define a variable a and assign 20 to it 
+print out all odd numbers from 1-100
+'''
