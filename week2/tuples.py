51869a2bf2d1ad8ea0b1c31f0b53460a85068c13
HassanBahati <mukisabahati@gmail.com>
2021-03-02 16:44:26 +0300

    introduction to tuples

---
+++ week2/tuples.py
@@ -0,0 +1,24 @@
+'''
+TUPLES
+tuples are ordered and unchangeable collections 
+tuples allow dupliate values
+'''
+a_tuple = (1,2,3,4,5)
+
+a_tuple = list(a_tuple)
+a_tuple.append(6)
+a_tuple = tuple(a_tuple)
+
+print(a_tuple)
+print(type(a_tuple))
+
+x_tuple = (10,11,12,13,14,15)
+y_tuple = x_tuple[:2]
+z_tuple = x_tuple[2:4]
+#to print out 10 and 11
+print(y_tuple)
+#to print out 12 and 13
+print(z_tuple)
+
+single_tuple = (90,)
+print(single_tuple)
