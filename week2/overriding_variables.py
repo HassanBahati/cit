3f93fa898c1a7746ff6b39c59112dbb0096b0d18
HassanBahati <mukisabahati@gmail.com>
2021-03-04 17:16:22 +0300

    overiding varibles & int

---
+++ week2/overriding_variables.py
@@ -0,0 +1,24 @@
+#overriding variables
+name = 'bahati'
+print(name)
+
+name = 'jacob'
+print(name)
+
+#the most recent varibale assignment 
+name = 'tweety'
+print(type(name))
+print(name)
+
+#override integers (int)
+age = 10
+print(age)
+
+age = 30
+print(age)
+
+#the most recent age variable assignment
+age = 100
+print(age)
+print(type(age))
+
