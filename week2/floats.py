a0775a3855dc2c0f7241161c45c6c1b7c8f9a6d9
HassanBahati <mukisabahati@gmail.com>
2021-03-04 16:59:44 +0300

    edited floats.py

---
+++ week2/floats.py
@@ -0,0 +1,19 @@
+#floats
+f1 = 2.5 #floating point number
+i = 2 #integer
+
+#multiplying
+g = f1 * i
+print(g)
+
+#adding
+h = f1 + i 
+print(h)
+
+#diving
+i = f1 / i
+print(i)
+
+#subtracting
+j = f1 - i
+print(j)
