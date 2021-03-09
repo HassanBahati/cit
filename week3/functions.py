da01ccdc31ac45ca1c4bceae16d5f2789a27eef5
HassanBahati <mukisabahati@gmail.com>
2021-03-09 16:45:33 +0300

    py functions

---
+++ week3/functions.py
@@ -0,0 +1,17 @@
+#functions :: a & b are function parameters
+def sum(a, b):
+    print(a + b)
+
+#run the function
+sum(4, 3)
+
+# c = 5 + sum(2, 2) #TypeError: unsupported operand type(s) for +: 'int' and 'Nfunctions.py", line 8, in <module>oneType'
+          
+
+
+def return_sum(a, b):
+    return a + b
+
+print(type(return_sum(2,2)))
+
+
