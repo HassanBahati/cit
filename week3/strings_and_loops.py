5f5ae41a1afa90efa6e5bea0129736b0eb849157
HassanBahati <mukisabahati@gmail.com>
2021-03-10 18:20:15 +0300

    strings_and_loops

--- week3/strings_and_loops.py
+++ week3/strings_and_loops.py
@@ -1,1 +1,22 @@
 #atring manipulation
+
+cold = 'Antarctica'
+
+#indexing starts from 0 - (len(N) - 1)
+#where n is the string 
+
+l = len(cold)
+
+#string formatting 
+f = f'{cold} is a string of length {l}'
+
+print(f)
+
+print(len(cold))
+
+#prints first letter in string 
+print(cold[0])
+
+
+day = 'how was your day'.format(day='monday', name='jane')
+print(day)
