5f23a09401a34bff81f0c80cb5ce2395b31fa5e8
HassanBahati <mukisabahati@gmail.com>
2021-03-10 19:06:00 +0300

    slicing

---
+++ week3/arrays.py
@@ -0,0 +1,20 @@
+#arrays : list 
+l = ['mon', 'tues', 'wed', 'thurs',
+'fir', 'sat', 'sun']
+
+print(l)
+
+#slicing example 
+cow = 'moo'
+#print index one onwards
+print(cow[1:])
+
+#string indexing example
+print(cow[0])
+
+#try printing out sat and sun in different ways
+#printing sun and sat
+print(l[5:])
+
+#printing sun and sat using reverse indexing
+print(l[-2:])
