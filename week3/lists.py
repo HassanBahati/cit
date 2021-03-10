a3b23c78b19fddeb644bfadd4f338c6e735325ed
HassanBahati <mukisabahati@gmail.com>
2021-03-10 19:20:24 +0300

    created lists.py

---
+++ week3/lists.py
@@ -0,0 +1,11 @@
+#2d list
+
+mountains = [['Ngalima', 16762], ['Elgon', 14177], 
+            ['Speke', 16043], ['Muhavura', 13540], 
+             ['Gessi', 15469], ['Moroto', 10115], 
+             ['Kadam', 10049]]
+
+print(mountains)
+
+
+
