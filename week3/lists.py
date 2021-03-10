4228bf1e44c1b659e226bdaa0610bfd56d7fbd6c
HassanBahati <mukisabahati@gmail.com>
2021-03-10 19:32:15 +0300

    edited lists.py

--- week3/lists.py
+++ week3/lists.py
@@ -2,10 +2,16 @@
 
 mountains = [['Ngalima', 16762], ['Elgon', 14177], 
             ['Speke', 16043], ['Muhavura', 13540], 
-             ['Gessi', 15469], ['Moroto', 10115], 
+             ['Gessi', 15469], ['Moroto', 10115, True], 
              ['Kadam', 10049]]
 
 print(mountains)
 
+# for x in mountains:
+#     if 'Kadam' in mountains:
+#         print('yes')
+#     else:
+#         print('no')
 
+print(mountains[5][1])
 
