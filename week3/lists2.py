f9f7ed066437de888bcb6f327a913a7b503fd954
HassanBahati <mukisabahati@gmail.com>
2021-03-12 18:33:34 +0300

    created list2.py  :white_check_mark:

---
+++ week3/lists2.py
@@ -0,0 +1,15 @@
+name_list = [['Ivan', 15], ['Ahabyona', 70]]
+
+print(len(name_list)) # what will this print out?
+print(name_list[0][1])
+
+
+
+mountains = [['Ngaliema',16762], ['Elgon',14177],
+             ['Speke',16043], ['Muhavura',13540],
+             ['Gessi',15469], ['Moroto',10115],
+             ['Kadam',10049]] # 2D list
+
+
+
+
