32ad37f1201d39ac94a1b17383069a1f984ecdae
HassanBahati <mukisabahati@gmail.com>
2021-03-19 16:14:21 +0300

    added week 4

---
+++ week3/urlencoded.py
@@ -0,0 +1,10 @@
+def String_Encoder(s):
+    #start here
+    e = ''
+    for iterator in range(len(s)):
+        if s[-terator-1] == 'a':
+            print('z')
+        print(s[-iterator-1])
+    return __
+
+String_Encoder('Its a rainy day')
