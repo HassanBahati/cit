105cfbf5c40395da17a209c95c983728c9d072ea
HassanBahati <mukisabahati@gmail.com>
2021-03-19 17:08:26 +0300

    :rocket:

--- week4/dict.py
+++ week4/dict.py
@@ -5,15 +5,16 @@ employees = {'id': [201, 600],
 
 
 #print out all values in the dict
-print(employees['id'])
-print(employees['name'])
-print(employees['bonus'])
+# print(employees['id'])
+# print(employees['name'])
+# print(employees['bonus'])
 
 #print out all list values interatively now
+ids = ['id', 'name', 'bonus']
 for i in range(len(employees)):
-    print(employees[i])
+    print(employees[ids[i]])
 
 #add 'location' to the employees dictionary location:('usa' , 'uganda')
-print(employees)
+
 
 #we will go over froup projects soon
