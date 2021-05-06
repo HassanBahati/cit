c109eb25fb9486e122d6faab84a7fe5dfe201ac5
HassanBahati <mukisabahati@gmail.com>
2021-05-06 14:40:18 +0300

    added loop over multi dimesnsional list

--- week9/edube.practice/for_loops.py
+++ week9/edube.practice/for_loops.py
@@ -24,3 +24,9 @@ print(next(itr))
 
 #range
 print(list(range(0,10,2)))
+
+#cycle through multi dimemsional list- prints all numbers 
+num_list=[[1,2,3],[10,20,30],[100,200,300]]
+for x in range(0,3):
+    for y in range(0,3):
+        print(num_list[x][y])
