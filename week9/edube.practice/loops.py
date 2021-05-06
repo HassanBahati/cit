80f271d84631ab4ed142501f45080f8289cee966
HassanBahati <mukisabahati@gmail.com>
2021-05-06 14:26:16 +0300

    added while loops

--- week9/edube.practice/loops.py
+++ week9/edube.practice/loops.py
@@ -6,3 +6,19 @@ while w1<5:
     w1+=1
 
 #to print even numbers
+#break-stop loopoing   
+w2=0
+while w2 <= 20:
+    if w2 % 2 == 0:
+        print(w2)
+    elif w2 == 9:
+        break
+    else:
+        w2+= 1
+        continue
+    w2 += 1
+
+#  looping through lists
+l4=[1,3,3.14,"Derrick"]
+while len(l4):
+    print(l4.pop(0))
