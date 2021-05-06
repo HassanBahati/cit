c41746e2d9cc054bedeec8d52996c85c77c8077f
HassanBahati <mukisabahati@gmail.com>
2021-05-06 14:31:35 +0300

    for loop

--- week9/edube.practice/for_loops.py
+++ week9/edube.practice/for_loops.py
@@ -4,3 +4,12 @@
 for x in range(0,10):
     print(x, "", end='')
 print()
+
+#cylce througha  list 
+l4=[1,3,'Derrick']
+for x in l4:
+    print(x)
+
+#cycle through a defined list
+for x in [2,4,6]:
+    print(x  )
