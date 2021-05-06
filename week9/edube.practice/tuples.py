1a5cfd38938e21736a9a92fa6525a91eecf62c45
HassanBahati <mukisabahati@gmail.com>
2021-05-06 14:47:06 +0300

    manipulation of lists

---
+++ week9/edube.practice/tuples.py
@@ -0,0 +1,8 @@
+#anyting done to a list can happen with a tuplee except changing a value 
+
+t1=(1,3.14,'Derrick')
+print('length', len(t1))
+print('1st', t1[0])
+print('lst', t1[-1])
+print('every other', t1[0:-1:2])
+print('reverse', t1[::-1])
