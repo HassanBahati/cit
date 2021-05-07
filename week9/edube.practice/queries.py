2459b7a630576070c3ed230585a55aef5c23c34c
HassanBahati <mukisabahati@gmail.com>
2021-05-07 15:12:24 +0300

    added indexing to indexed loop

--- week9/edube.practice/queries.py
+++ week9/edube.practice/queries.py
@@ -1,6 +1,6 @@
-#print(x) prints the keys while print(dit[x]) prints the values 
+#print(x) prints the keys while print(dit[x]) prints the values then when an index is added it prints only the indexed
 dit={}
 dit['1']=(3,4)
 dit['2']=(4,5)
 for x in dit.keys():
-    print(dit[x])
+    print(dit[x][1], end='')
