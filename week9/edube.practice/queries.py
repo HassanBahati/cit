316eb4594bc362858ef8552ba3f5fd6f334ad723
HassanBahati <mukisabahati@gmail.com>
2021-05-07 15:30:49 +0300

    added modulo

--- week9/edube.practice/queries.py
+++ week9/edube.practice/queries.py
@@ -5,7 +5,23 @@ dit['2']=(4,5)
 for x in dit.keys():
     print(dit[x][1], end='')
 
+#lists
 my_list=[1,2]
 for v in range(2):
     my_list.insert(-1, my_list[v])
 print(my_list)
+
+#
+my_list=[x*x for x in range(5)]
+def func(lst):
+    del lst[lst[2]]
+    return lst
+print(func(my_list))
+
+#modulo
+x=int(input())
+y=int(input())
+x=x%y
+x=x%y
+y=y%x
+print(y)
