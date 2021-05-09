8ac97cdb18c51d251277f171e8f4a99e9ebde595
HassanBahati <mukisabahati@gmail.com>
2021-05-09 20:08:04 +0300

    checked speed of np array

--- week10/numpy_crash.py
+++ week10/numpy_crash.py
@@ -22,3 +22,23 @@ print(sys.getsizeof(5)*len(S))
 D=np.arange(1000)
 print(D.size*D.itemsize)
 
+# #testng speed and conviency of umpy vs lists
+# SIZE=1000
+
+# L1=range(SIZE)
+# L2=range(SIZE)
+
+# #calculating sum in lists
+# A1=np.arange(SIZE)
+# A2=np.arange(SIZE)
+
+# start = time.time()
+
+# result= [(x,y) for x,y in zip(L1,L2)]
+
+# print((time.time()-start)*1000)
+
+# #for numpy 
+# start=time.time()
+# result=A1+A2
+# print((time.time()-start)*1000) 
