27e56949ff001b38eefa43c84103509be3afd907
HassanBahati <mukisabahati@gmail.com>
2021-05-09 21:21:17 +0300

    numpy math

---
+++ week10/numpy_math.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+'''
+
+'''
+
+a=np.array([(1,2,3),(3,4,5)])
+
+b=np.array([(1,2,3),(3,4,5)])
+ 
+#sum of the arrays
+print(a+b)
+
+#product
+print(a*b)
+
+#division
+print(a/b)
