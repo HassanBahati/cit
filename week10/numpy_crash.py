32e9e0a7707baffc5918a71d8be3764430ff8ea9
HassanBahati <mukisabahati@gmail.com>
2021-05-09 19:56:47 +0300

    proved size of numpy vs lists

--- week10/numpy_crash.py
+++ week10/numpy_crash.py
@@ -1,4 +1,10 @@
 import numpy as np 
+import sys
+import time
+'''
+occupies lless spcae than lists in memory
+faster than lists and more convient
+'''
 
 #prining a list- single dimensional array 
 a=np.array([1,2,3]) 
@@ -7,3 +13,12 @@ print(a)
 #2-dimensional array
 a=np.array([(1,2,3),(4,5,6)])
 print(a)
+
+#getting spcae occupied by s
+S = range(1000)
+print(sys.getsizeof(5)*len(S))
+
+#spce occupied by same but in numpy
+D=np.arange(1000)
+print(D.size*D.itemsize)
+
