f7ba5613489c77f5e872fdb760b91fdf6758d720
HassanBahati <mukisabahati@gmail.com>
2021-05-09 22:20:30 +0300

    exponetials and logs

---
+++ week10/exponential_and_log.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+ar=np.array([1,2,3])
+
+#exponetial
+print(np.exp(ar))
+
+#natural log -log base e (ln)
+print(np.log(ar))
+
+#log base 10
+print(np.log10(ar))
--- week10/special_functions.py
+++ week10/special_functions.py
@@ -1,9 +1,11 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
-
+'''
+sin,tan,cos
+'''
 x=np.arange(0,3*np.pi,0.1)
 
-y=np.sin(x)
+y=np.tan(x)
 
 #plotting the graph
 plt.plot(x,y)
